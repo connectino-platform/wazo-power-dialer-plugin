c1a01666303f5c45f36a64dd4d328dfaa95e1df6
farhad2161 <farhad2161@yahoo.com>
2022-08-15 10:05:44 +0430

    Update campaign model and schema

--- wazo_confd_power_dialer/campaign/model.py
+++ wazo_confd_power_dialer/campaign/model.py
@@ -2,14 +2,23 @@ from sqlalchemy import (
     Column
 )
 from sqlalchemy import text
-from sqlalchemy.types import String
+from sqlalchemy.types import (String, Boolean, Date, Time, Integer)
 from xivo_dao.helpers.db_manager import UUIDAsString
 
 from ..db import Base
 
 
 class CampaignModel(Base):
-    __tablename__ = 'plugin_campaign'
+    __tablename__ = 'plugin_powerdialer_campaign'
 
     uuid = Column(UUIDAsString(36), primary_key=True, server_default=text('uuid_generate_v4()'))
     name = Column(String(128), nullable=False)
+    status = Column(String(128), nullable=False)
+    schedule_start_date = Column(Date, nullable=True)
+    schedule_start_time = Column(Time, nullable=True)
+    answer_wait_time = Column(Integer, nullable=False)
+    after_call_dialing_auto = Column(Boolean, nullable=False)
+    after_call_time = Column(Integer, nullable=False)
+    is_recording = Column(Boolean, nullable=False)
+    attempts = Column(Integer, nullable=False)
+    attempts_interval = Column(Integer, nullable=False)
--- wazo_confd_power_dialer/campaign/schema.py
+++ wazo_confd_power_dialer/campaign/schema.py
@@ -1,7 +1,17 @@
 from marshmallow import fields
+from marshmallow.validate import OneOf
 from wazo_confd.helpers.mallow import BaseSchema
 
 
 class CampaignSchema(BaseSchema):
     uuid = fields.Str(dump_only=True)
     name = fields.Str(required=True)
+    status = fields.String(validate=OneOf(['active', 'inactive']), allow_none=False)
+    schedule_start_date = fields.Date()
+    schedule_start_time = fields.Time()
+    answer_wait_time = fields.Integer(required=False, default=60)
+    after_call_dialing_auto = fields.Boolean(required=False, default=True)
+    after_call_time = fields.Integer(required=False, default=0)
+    is_recording = fields.Boolean(required=False, default=False)
+    attempts = fields.Integer(required=False, default=1)
+    attempts_interval = fields.Integer(required=False, default=10)
